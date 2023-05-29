9f6df9737d4780672ee5827a56ff2733cc0d473f
Dmitriy <dmitrypolyakovd@gmail.com>
2023-05-29 16:01:11 +0300

    сумма

---
+++ task_66/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите начальное значение (M): ");
+        int M = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите конечное значение (N): ");
+        int N = int.Parse(Console.ReadLine());
+
+        int sum = GetSumOfNaturalNumbers(M, N);
+        Console.WriteLine($"Сумма натуральных чисел от {M} до {N}: {sum}");
+    }
+
+    static int GetSumOfNaturalNumbers(int start, int end)
+    {
+        int sum = 0;
+
+        for (int i = start; i <= end; i++)
+        {
+            sum += i;
+        }
+
+        return sum;
+    }
+}

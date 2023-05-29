73689d62443edff69e1cae1541517bb8bce3e1aa
Dmitriy <dmitrypolyakovd@gmail.com>
2023-05-29 15:55:16 +0300

    задача 64

---
+++ task_64/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int M = 1; // Начальное значение промежутка
+        int N = 10; // Конечное значение промежутка
+
+        Console.WriteLine($"Натуральные числа от {M} до {N}:");
+        PrintNaturalNumbers(M, N);
+    }
+
+    static void PrintNaturalNumbers(int start, int end)
+    {
+        if (start > end)
+        {
+            return;
+        }
+
+        Console.WriteLine(start);
+        PrintNaturalNumbers(start + 1, end);
+    }
+}
